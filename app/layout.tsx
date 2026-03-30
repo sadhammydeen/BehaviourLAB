cc705297038c635d1e12b8d3daed920c8043ad48
sadhammydeen <sadhammydeen@gmail.com>
2026-03-30 18:20:40 +0530

    Update all icons and favicon with new design

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,11 +33,13 @@ export const metadata: Metadata = {
         type: 'image/svg+xml',
       },
       {
-        url: '/icon-light-32x32.png',
+        url: '/icon-light-32x32.svg',
+        type: 'image/svg+xml',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/icon-dark-32x32.svg',
+        type: 'image/svg+xml',
         media: '(prefers-color-scheme: dark)',
       },
       {
@@ -45,7 +47,7 @@ export const metadata: Metadata = {
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '/icon.svg',
   },
 }
 
